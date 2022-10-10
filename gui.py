9d0b041fca1511fe3cfd2e613ba1a729fbae0acb
ghostySeven7 <101484221+ghostySeven7@users.noreply.github.com>
2022-10-10 18:13:45 -0400

    not done

--- gui.py
+++ gui.py
@@ -7,77 +7,56 @@ import time
 
 # defining the master gui box
 master = tk.Tk()
-master.geometry('300x300')
+master.geometry('500x500')
 master.title('Inventory-Live Application')
 
 
 # creating start button image
 start_image = Image.open('/Users/jamesbaxter/Desktop/tab_inven/start_pic.png')
-start_image = start_image.resize((25, 25), Image.ANTIALIAS)
+start_image = start_image.resize((100, 80), Image.ANTIALIAS)
 start_image = ImageTk.PhotoImage(start_image)
 
 
 # creating kill button image
 kill_image = Image.open('/Users/jamesbaxter/Desktop/tab_inven/stop_pic.png')
-kill_image = kill_image.resize((25, 25), Image.ANTIALIAS)
+kill_image = kill_image.resize((100, 80), Image.ANTIALIAS)
 kill_image = ImageTk.PhotoImage(kill_image)
 
 
-# "Inventory-Live Application Interface Menu"
-label_title = tk.Label(master, text='Inventory-Live Application Interface Menu')
-label_title.place(relx=0.5, rely=0.2, anchor='center')
+# creating status graphic image
+status_image = Image.open('/Users/jamesbaxter/Desktop/tab_inven/status_pic.png')
+status_image = status_image.resize((300, 60), Image.ANTIALIAS)
+status_image = ImageTk.PhotoImage(status_image)
 
 
 # Program status module (passive [dumb]... for now...)
-def button_envr_callback():
-    if label_envr['text'] == 'Ready':
-        label_envr['text'] = 'Running'
-        messagebox.showinfo('System_Status:', 'Envr Control Now Running.')
-    else:
-        label_envr['text'] = 'Ready'
-
-
-# label and button entities
-label_envr = tk.Label(master, text='Ready')
-button_envr = tk.Button(master, text='  Envr Ctrl  ', image=start_image, command=button_envr_callback, compound=tk.TOP)
-
-# label and button positions & plotting
-label_envr.place(relx=0.7, rely=0.4, anchor='center')
-button_envr.place(relx=0.4, rely=0.4, anchor='center')
-
-
-# Auto-Harvest control interface #
-def button_harvest_callback():
-    if label_harvest['text'] == 'Ready':
-        label_harvest['text'] = 'Running'
-        messagebox.showinfo('System_Status:', 'Harvest Now Running.')
-    else:
-        label_envr['text'] = 'Ready'
-
-
-# label and button entities
-label_harvest = tk.Label(master, text='Ready')
-button_harvest = tk.Button(master, text='  Harvest   ', image=start_image, command=button_harvest_callback, compound=tk.TOP)
-
-# label and button positions & plotting
-label_harvest.place(relx=0.7, rely=0.6, anchor='center')
-button_harvest.place(relx=0.4, rely=0.6, anchor='center')
+def start_button_click():
+    start_status['text'] = 'Running'
 
 
 # Universal kill-switch button interface #
-def button_kill_callback():
-    label_envr['text'] = 'Ready'
-    label_harvest['text'] = 'Ready'
-    messagebox.showinfo('System_Status:', 'Process Killed Successfully')
+def stop_button_click():
+    start_status['text'] = 'Ready'
 
 
-# label and button entities
-button_kill = tk.Button(master, text='Kill Switch ', image=kill_image, command=button_kill_callback, compound=tk.TOP)
-# label_kill = tk.Label(master, text=' ')
+# START button properties
+start_button = tk.Button(master, image=start_image, borderwidth=20,  command=start_button_click, compound=tk.TOP)
+start_button.place(relx=0.3, rely=0.2, anchor='center')
 
-# label and button positions & plotting
-# label_kill.grid(row=2, column=1)
-button_kill.place(relx=0.4, rely=0.8, anchor='center')
+
+# STOP button properties
+stop_button = tk.Button(master, image=kill_image, borderwidth=20, command=stop_button_click, compound=tk.TOP)
+stop_button.place(relx=0.7, rely=0.2, anchor='center')
+
+
+# STATUS text properties
+start_status = tk.Label(master, text='Ready')
+start_status.place(relx=0.5, rely=0.6, anchor='center')
+
+
+# STATUS graphic properties
+status_graphic = tk.Label(master, image=status_image, compound=tk.TOP)
+status_graphic.place(relx=0.5, rely=0.5, anchor='center')
 
 
 # the app mainloop
