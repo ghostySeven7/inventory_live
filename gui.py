794f5a1dfa2fba5dcf5c34718756b19dc3c9f8c4
ghostySeven7 <101484221+ghostySeven7@users.noreply.github.com>
2022-10-19 18:10:22 -0400

    completed v2

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from PIL import Image, ImageTk  # as 'pillow'
-import live_inventory
+import inventory_live
 
 
 # defining the master gui box
@@ -11,15 +11,15 @@ master.title('Inventory-Live Application')
 
 
 # creating run button image
-run_image = Image.open('/Users/jamesbaxter/PycharmProjects/finances/run_pic.png')  # change at production
+run_image = Image.open('/Users/jamesbaxter/PycharmProjects/finances/sync_pic.png')  # change at production
 run_image = run_image.resize((300, 100), Image.ANTIALIAS)
 run_image = ImageTk.PhotoImage(run_image)
 
 
-# runs live_inventory.py
+# runs merge_inventory.py
 def run_button_click():
-    messagebox.showinfo('System_Status:', 'Inventory has been updated')
-    live_inventory.makes_ledger_live()
+    messagebox.showinfo('System_Status:', 'Inventory has been synchronized')
+    inventory_live.makes_ledger_live()
 
 
 # RUN button properties
@@ -28,6 +28,4 @@ run_button.place(relx=0.5, rely=0.5, anchor='center')
 
 
 tk.mainloop()
-
-
 
